ba3598ae97e925d0de38289a7ca4857b305d7a38
Damir Bozic <ferari88>
2017-12-19 20:39:07 -0500

    connected to database

--- app/src/main/java/com/strobertchs/cptassignment/ListofTasks.java
+++ app/src/main/java/com/strobertchs/cptassignment/ListofTasks.java
@@ -1,4 +1,4 @@
-package com.strobertchs.mytimecpt;
+package com.strobertchs.cptassignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class ListofTasks extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listof_tasks);
+        setContentView(com.strobertchs.mytimecpt.R.layout.activity_listof_tasks);
     }
 }
--- app/src/main/java/com/strobertchs/cptassignment/MainActivity.java
+++ app/src/main/java/com/strobertchs/cptassignment/MainActivity.java
@@ -2,6 +2,12 @@ package com.strobertchs.cptassignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String url = "jdbc:postgresql://ec2-54-235-244-185.compute-1.amazonaws.com:5432/";
+        Properties props = new Properties();
+        props.setProperty("user","wgibtmqeiltsdf");
+        props.setProperty("password","b3ca2821037ac3b7354d13bf6a72291431b5d73a92e5fd03b3c9749ffb460360");
+        try {
+            Connection conn = DriverManager.getConnection(url, props);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
