b69a4fc07ecc76166d57fce7363d035304361884
emanresu24 <ali.hedayatishalkouhi18@ycdsbk12.ca>
2018-01-25 12:48:35 -0500

    Map init

---
+++ app/src/main/java/com/strobertchs/cptassignment/Mainschoolmap.java
@@ -0,0 +1,13 @@
+package com.strobertchs.cptassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mainschoolmap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainschoolmap);
+    }
+}
