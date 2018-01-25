1c8b3241ec6a9262cfaf6f069c9e7424a7e4abf2
Damir Bozic <ferari88>
2018-01-24 19:30:05 -0500

    added back buttons to all the importabt screens

--- app/src/main/java/com/strobertchs/cptassignment/MainActivity.java
+++ app/src/main/java/com/strobertchs/cptassignment/MainActivity.java
@@ -2,6 +2,7 @@ package com.strobertchs.cptassignment;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -13,7 +14,8 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
-
+    @LayoutRes
+    private int backlayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,18 +37,20 @@ public class MainActivity extends AppCompatActivity {
     public void onChooseSignIn(View view){
 
         setContentView(R.layout.activity_sign_in_screen);
-
+        setbacklayout(R.layout.activity_opening__screen);
     }
 
     public void onChooseRegister(View view){
 
         setContentView(R.layout.activity_register_screen);
+        setbacklayout(R.layout.activity_opening__screen);
 
     }
 
     public void onChooseRegtoSign(View view){
 
         setContentView(R.layout.activity_sign_in_screen);
+        setbacklayout(R.layout.activity_opening__screen);
 
     }
     public void onChooseSign2app(View view){
@@ -59,23 +63,23 @@ public class MainActivity extends AppCompatActivity {
     public void onwednesday(View view){
 
         setContentView(R.layout.activity_wednesday);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setbacklayout(R.layout.activity_main_screen);
     }
     public void onmonday(View view){
         setContentView(R.layout.activity_monday);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setbacklayout(R.layout.activity_main_screen);
     }
     public void ontuesday(View view){
         setContentView(R.layout.activity_tuesday);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setbacklayout(R.layout.activity_main_screen);
     }
     public void onthursday(View view){
         setContentView(R.layout.activity_thursday);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setbacklayout(R.layout.activity_main_screen);
     }
     public void onfriday(View view){
         setContentView(R.layout.activity_friday);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setbacklayout(R.layout.activity_main_screen);
     }
     public void onschoolsite(View view){
         Intent link = new Intent(Intent.ACTION_VIEW,
@@ -92,10 +96,14 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == android.R.id.home){
-            setContentView(R.layout.activity_main_screen);
+            setContentView(backlayout);
             getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void setbacklayout(@LayoutRes int layout){
+        backlayout = layout;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
